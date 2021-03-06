e3562fec8de27c03c19805e168a46f273a81566c
Maleina Bidek <maleina@ucla.edu>
2021-03-06 10:57:56 -0500

    feat: Add recording screen

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording In Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
